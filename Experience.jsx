1ecb21c2a428c6075dbb3fa9b80703c19b13ebda
daoum101 <daoum@outlook.be>
2026-05-17 18:04:35 +0200

    Delete Experience.jsx

--- Experience.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function Experience() {
-  return (
-    <section className="experience">
-      <h2>Une vraie expérience italienne</h2>
-      <p>Terrasse, feu de bois, livraison locale et ambiance premium.</p>
-    </section>
-  )
-}
